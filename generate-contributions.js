51269994341d43e8209ac2bc08fd9e1490198aaf
HanryYu <fishion@outlook.com>
2023-06-27 10:34:33 +0800

    update

---
+++ generate-contributions.js
@@ -0,0 +1,18 @@
+const axios = require('axios');
+const fs = require('fs');
+
+async function generateContributions() {
+  try {
+    const response = await axios.get('https://green-wall.vercel.app/api/contribution/hanryyu');
+    const contributionsData = response.data;
+
+    fs.writeFileSync('date.json', JSON.stringify(contributionsData));
+
+    console.log('Contributions data updated successfully.');
+  } catch (error) {
+    console.error('Error updating contributions data:', error);
+    process.exit(1);
+  }
+}
+
+generateContributions();
